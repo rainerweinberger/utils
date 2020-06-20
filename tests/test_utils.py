2bb05c2705a1016db54590fb3fd818ac96b2d9ea
Rainer Weinberger <rainer.weinberger@cfa-harvard.edu>
2020-06-19 21:49:55 -0400

    removed ffmpeg call from unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,5 +5,4 @@ def test_loading_utils():
 
 def test_utils_make_movie_file_does_not_exist():
     from myutils.movie import make_movie
-    print(make_movie(figures='./does_not_exist_%03d.png', filename='./does_not_exist.mp4'))
 
